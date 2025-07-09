32c5a851ffc68b75392d223ea9bf2460b7fbb51b
elpollas1111 <aitorcitomasa10@gmail.com>
2025-07-09 23:29:29 +0200

    Create Constants.cs

---
+++ MBRReader/Constants.cs
@@ -0,0 +1,6 @@
+internal static class Constants {
+    public const uint GENERIC_READ = 0x80000000;
+    public const uint FILE_SHARE_READ = 0x00000001;
+    public const uint FILE_SHARE_WRITE = 0x00000002;
+    public const uint OPEN_EXISTING = 3;
+}
